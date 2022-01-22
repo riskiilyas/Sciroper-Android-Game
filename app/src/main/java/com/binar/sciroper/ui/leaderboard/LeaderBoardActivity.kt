5ad892e299bac466842a88acc380ab85aa527ba2
Rahmathidayat4299 <hrahmat950@gmail.com>
2022-01-22 11:52:27 +0700

    update new

--- app/src/main/java/com/binar/sciroper/ui/leaderboard/LeaderBoardActivity.kt
+++ app/src/main/java/com/binar/sciroper/ui/leaderboard/LeaderBoardActivity.kt
@@ -2,6 +2,9 @@ package com.binar.sciroper.ui.leaderboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.binar.sciroper.data.db.user.User
 import com.binar.sciroper.databinding.ActivityLeaderBoardBinding
 
 class LeaderBoardActivity : AppCompatActivity() {
@@ -9,6 +12,11 @@ class LeaderBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val recyclerView = binding.rcPlayer
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = Adapter(this, User as MutableList<User>)
 
     }
 }
