f3f0b36bb33b330ef2f0eee7ca96922ecf29e12f
Rahmat <sugiartorahmat187@gmail.com>
2022-01-24 22:36:01 +0700

    fix bug leaderboard

--- app/src/main/java/com/binar/sciroper/ui/leaderboard/LeaderBoardActivity.kt
+++ app/src/main/java/com/binar/sciroper/ui/leaderboard/LeaderBoardActivity.kt
@@ -2,6 +2,7 @@ package com.binar.sciroper.ui.leaderboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import com.binar.sciroper.data.db.AppDB
@@ -20,13 +21,32 @@ class LeaderBoardActivity : AppCompatActivity(), MainView {
         setContentView(binding.root)
         presenterMain = PresenterBoard(this)
 
-        presenterMain.getData().observe(this,{
-            binding.tvRankDua.text = UserLevel.sortUsersLevel(it)[1].username
-            binding.rankDua.setImageResource(UserLevel.sortUsersLevel(it)[1].avatarId)
-            binding.tvRankSatu.text = UserLevel.sortUsersLevel(it)[0].username
-            binding.rankSatu.setImageResource(UserLevel.sortUsersLevel(it)[0].avatarId)
-            binding.tvRankTiga.text = UserLevel.sortUsersLevel(it)[2].username
-            binding.rankTiga.setImageResource(UserLevel.sortUsersLevel(it)[2].avatarId)
+        presenterMain.getData().observe(this, {
+            if (it.size <= 1) {
+                binding.tvRankSatu.text = UserLevel.sortUsersLevel(it)[0].username
+                binding.rankSatu.setImageResource(UserLevel.sortUsersLevel(it)[0].avatarId)
+                binding.tvRankDua.visibility = View.INVISIBLE
+                binding.tvRankTiga.visibility = View.INVISIBLE
+                binding.ivRank2.visibility = View.INVISIBLE
+                binding.ivRank3.visibility = View.INVISIBLE
+
+            } else if (it.size <= 2) {
+                binding.tvRankSatu.text = UserLevel.sortUsersLevel(it)[0].username
+                binding.rankSatu.setImageResource(UserLevel.sortUsersLevel(it)[0].avatarId)
+                binding.tvRankDua.text = UserLevel.sortUsersLevel(it)[1].username
+                binding.rankDua.setImageResource(UserLevel.sortUsersLevel(it)[1].avatarId)
+                binding.tvRankTiga.visibility = View.INVISIBLE
+                binding.ivRank3.visibility = View.INVISIBLE
+            } else {
+                binding.tvRankSatu.text = UserLevel.sortUsersLevel(it)[0].username
+                binding.rankSatu.setImageResource(UserLevel.sortUsersLevel(it)[0].avatarId)
+                binding.tvRankDua.text = UserLevel.sortUsersLevel(it)[1].username
+                binding.rankDua.setImageResource(UserLevel.sortUsersLevel(it)[1].avatarId)
+                binding.tvRankTiga.text = UserLevel.sortUsersLevel(it)[2].username
+                binding.rankTiga.setImageResource(UserLevel.sortUsersLevel(it)[2].avatarId)
+            }
+
+
         })
         presenterMain.getData().observe(this, {
             val recyclerView = binding.rcPlayer
