8f121a30c86e4f2aa7487e0933da876bcd4959e9
hafiz <hafizski123@gmail.com>
2022-01-24 22:54:31 +0700

    2nd commit

---
+++ app/src/main/java/com/binar/sciroper/ui/how_to_play/HowToPlay.kt
@@ -0,0 +1,11 @@
+package com.binar.sciroper.ui.how_to_play
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HowToPlay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_how_to_play)
+    }
+}
---
+++ app/src/main/java/com/binar/sciroper/ui/setting/Setting.kt
@@ -0,0 +1,11 @@
+package com.binar.sciroper.ui.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Setting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
