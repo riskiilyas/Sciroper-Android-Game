5cb7cbf97e9ae5087968f75eaf68e1b33a37cdae
Rahmat <sugiartorahmat187@gmail.com>
2022-02-12 20:54:18 +0700

    fix-splahscreen

--- app/src/main/java/com/binar/sciroper/ui/fragments/menu/MenuFragment.kt
+++ app/src/main/java/com/binar/sciroper/ui/fragments/menu/MenuFragment.kt
@@ -35,8 +35,7 @@ class MenuFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         _binding = FragmentMenuBinding.inflate(inflater, container, false)
         return binding.root
     }
